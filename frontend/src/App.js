4f36ad96bc976135a1285758a04d95a2f2091aba
Aki Hasegawa-Johnson <104283523+aki0501@users.noreply.github.com>
2023-09-16 17:43:05 -0400

    Add navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navigation"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import "./styles/App.css"
+
 
 function App() {
+  let Component
+  switch (window.location.pathname) {
+    case "/":
+      Component = Home
+      break
+    case "/Profile":
+      Component = Profile
+      break
+  }
+
   return (
-    <div>
-    
-    </div>
+    <>
+      <Navbar />
+      <Component />
+    </>
   );
 }
 
