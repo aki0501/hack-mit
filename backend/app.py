a15a05053edfed30608da452b70e4a7d41bc35ca
benzuzu <ben.xu@yale.edu>
2023-09-16 18:05:34 -0400

    Add propublica api calls

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 
@@ -7,10 +8,13 @@ CORS(app)
 
 @app.route('/api/bills')
 def get_bills():
-    return jsonify([
-        {"id": 1, "name": "Bill A"},
-        {"id": 2, "name": "Bill B"}
-    ])
+    url = "https://api.propublica.org/congress/v1/bills/search.json?query=environment"
+    params = {
+        "api_key": "X-API-Key: Sz9Ma5gmzTRq2naj3qpqiAw91XveE7uvCefibHa7"
+    }
+
+    response = requests.get(url=url, params=params)
+    return response.json()
 
 if __name__ == '__main__':
     app.run(port=5000)
